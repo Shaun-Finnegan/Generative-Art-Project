edf37e03e3d63f632a20ad265c8284a112018f66
Shaun-Finnegan <sfinnegan.dev@gmail.com>
2023-07-25 16:43:30 +0000

    first fractal complete

--- script.js
+++ script.js
@@ -1,19 +1,76 @@
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1')
     const ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth * 0.8;
-    canvas.height = window.innerHeight * 0.8;
-    ctx.lineWidth = 30;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.lineWidth = 10;
     ctx.lineCap = 'round';
-    let size = 100;
-    ctx.rotate(0.1)
-    ctx.fillStyle = 'green'
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-    ctx.strokeStyle = 'yellow'
-    ctx.beginPath();
-    ctx.moveTo(canvas.width/2, canvas.height/2);
-    ctx.lineTo(size, canvas.height/2);
-    ctx.stroke();
+    ctx.shadowColor = 'rgba(0,0,0,0.7';
+    ctx.shadowOffsetX = 10;
+    ctx.shadowOffsetY = 5;
+    ctx.shadowBlur = 10;
+
+    let size = 200;
+    let sides = 5;
+    let maxLevel = 4;
+    let scale = 0.5;
+    let spread = 0.5;
+    let branches = 2;
+    let color = 'hsl('+ Math.random() * 360 +'0, 100%, 50%)';
+
+
+    ctx.fillStyle = 'green'
+    
+
+
+    function drawBranch(level){
+        if(level > maxLevel) return;
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.lineTo(size, 0);
+        ctx.stroke();
+        for (let i = 0; i < branches; i++){
+            ctx.save();
+            ctx.translate(size - (size/branches) * i, 0);
+            ctx.scale(scale, scale);
+            ctx.rotate(spread);
+            drawBranch(level + 1);
+            ctx.restore();
+    
+            ctx.save();
+            ctx.translate(size -(size/branches) * i, 0);
+            ctx.scale(scale, scale);
+            ctx.rotate(-spread);
+            drawBranch(level + 1);
+            ctx.restore();
+        }
+       
+    };
+
+   
+    
+function drawFractal (){
+    ctx.save();
+    ctx.strokeStyle = color;
+    ctx.translate(canvas.width/2, canvas.height/2);
+
+    for(i = 0; i < sides; i++){
+   
+    ctx.rotate((Math.PI * 2)/sides);
+    drawBranch(0);
+    
+};
+ctx.restore();
+};
+
+
+drawFractal();
+ 
+
+   
+
+
+    ctx.restore();
 
 });
